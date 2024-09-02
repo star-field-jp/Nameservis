fee487e7493eae47feb6fc3f88afcfef195ceda2
star-field-jp <hoshigari728@gmail.com>
2024-09-03 00:17:23 +0900

    Initial commit

---
+++ src/test/java/com/example/nameservis/NameservisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nameservis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NameservisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
